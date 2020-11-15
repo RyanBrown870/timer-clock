c8b71ff74d468ff4807280b495d7767a222e608d
rxb870 <ryanbrown0689@gmail.com>
2020-11-15 11:27:56 +0000

    Template clean-up

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import './App.css';
+import ReactFCCtest from 'react-fcctest';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
